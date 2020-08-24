b9cb5bc1964b132e1766a3cfa2be744c74c8e1b3
sunjian <sunjian@sensetime.com>
2020-08-24 16:21:34 +0800

    initial pod

--- A_Category/Classes/CTMediator+A.m
+++ A_Category/Classes/CTMediator+A.m
@@ -11,9 +11,6 @@
 @implementation CTMediator (A)
 - (UIViewController *)A_aViewController
 {
-    /*
-        AViewController *viewController = [[AViewController alloc] init];
-     */
     return [self performTarget:@"A" action:@"viewController" params:nil shouldCacheTarget:NO];
 }
 @end
